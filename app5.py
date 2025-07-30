6308ea1ae98dc9b7ec924c6a38e4acc832c42bb3
samyuktha-pinapati <samyukthapinapati@gmail.com>
2025-07-30 14:19:35 +0530

    python update

--- app5.py
+++ app5.py
@@ -1,11 +1,7 @@
-number=10
-scores=[10,20,30]  
-names=["alexa","siri","naseema"]
-for i in range(number):  #
-    print(i)
-
-for score in scores:
-    print(score)
-
-for name in names:
-    print(name)
+#print whether a number is even or odd
+number=int(input("enter your number"))
+#number=11
+if (number%2==0):
+    print("number is even")
+else:
+    print("number is odd")
